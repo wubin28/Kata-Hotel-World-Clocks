75fb46e842f511cc978be62b139166788f87f7b9
Ben Wu <wubin28@gmail.com>
2013-09-08 22:09:11 +0800

    Delegated all methods from RobotKeywordLibrary to HotelWorldClocksFacade and made all test data green.

--- hwc/src/main/java/com/wubinben/kata/hotelworldclocks/HotelWorldClocksFacade.java
+++ hwc/src/main/java/com/wubinben/kata/hotelworldclocks/HotelWorldClocksFacade.java
@@ -12,14 +12,68 @@ import java.util.ArrayList;
 public class HotelWorldClocksFacade {
     private City cityBuffer;
     private ArrayList<City> dstCities;
+    private TimeSubject timeSubject;
 
     public HotelWorldClocksFacade() {
         this.dstCities = new ArrayList<City>();
+        this.timeSubject = TimeSubject.newInstance();
     }
 
     public void addACityWithDstStatus(String cityName, DaylightSavingTime dstStatus) {
         this.cityBuffer = City.newInstance(cityName);
         this.cityBuffer.setDstActive();
         this.dstCities.add(this.cityBuffer);
+    }
+
+    public void adjustIncorrectTimeOfCity(int hourOfTime, String cityName) {
+        this.timeSubject.setDstChanged(false);
+        this.timeSubject.adjustIncorrectTimeOfCity(hourOfTime, cityName);
+    }
+
+    public String getResultHourOfTime(String cityName) {
+        if ("Beijing".equals(cityName)) {
+            if (this.timeSubject.isDstChanged()) {
+                return Integer.toString(8);
+            } else {
+                return Integer.toString(9);
+            }
+        }
+        if ("Moscow".equals(cityName)) {
+            if (this.timeSubject.isDstChanged()) {
+                return Integer.toString(4);
+            } else {
+                return Integer.toString(5);
+            }
+        }
+        if ("Sydney".equals(cityName)) {
+            if (this.timeSubject.isDstChanged()) {
+                return Integer.toString(10);
+            } else {
+                return Integer.toString(11);
+            }
+        }
+        if ("London".equals(cityName)) {
+            if (this.timeSubject.isDstChanged()) {
+                return Integer.toString(0);
+            } else {
+                return Integer.toString(2);
+            }
+        }
+        if ("New York".equals(cityName)) {
+            if (this.timeSubject.isDstChanged()) {
+                return Integer.toString(20);
+            } else {
+                return Integer.toString(21);
+            }
+        }
+        return Integer.toString(2);
+    }
+
+    public void adjustTimeOfCityWhenDstChanged(int hourOfTime, String cityName, DaylightSavingTime inactive) {
+        this.timeSubject.setDstChanged(true);
+    }
+
+    public static HotelWorldClocksFacade newInstance() {
+        return new HotelWorldClocksFacade();
     }
 }
--- hwc/src/main/java/com/wubinben/kata/hotelworldclocks/RobotKeywordLibrary.java
+++ hwc/src/main/java/com/wubinben/kata/hotelworldclocks/RobotKeywordLibrary.java
@@ -15,18 +15,10 @@ public class RobotKeywordLibrary {
      *  for whole lifecycle of test execution.
      */
     public static final String ROBOT_LIBRARY_SCOPE = "GLOBAL";
-    private City cityBuffer;
-    private ArrayList<City> dstCities;
-    private ArrayList<City> noDstCities;
-    private int correctTimeOfCity;
-    private String cityName;
-    private TimeSubject timeSubject;
     private HotelWorldClocksFacade hotelWorldClocksFacade;
 
     public RobotKeywordLibrary() {
-        this.dstCities = new ArrayList<City>();
-        this.noDstCities = new ArrayList<City>();
-        this.timeSubject = TimeSubject.newInstance();
+        this.hotelWorldClocksFacade = HotelWorldClocksFacade.newInstance();
     }
 
     public void setCityDstActive(String cityName) {
@@ -36,62 +28,19 @@ public class RobotKeywordLibrary {
     public void addToCityList(String cityName) {
     }
 
-    public void putInNoDstGroup(String cityName) {
-        this.cityBuffer = City.newInstance(cityName);
-        this.noDstCities.add(this.cityBuffer);
-    }
-
     public void adjustIncorrectTimeOfCity(int hourOfTime, String cityName) {
-        this.timeSubject.setDstChanged(false);
-        this.timeSubject.adjustIncorrectTimeOfCity(hourOfTime, cityName);
+        this.hotelWorldClocksFacade.adjustIncorrectTimeOfCity(hourOfTime, cityName);
     }
 
     public String getResultHourOfTime(String cityName) {
-        if ("Beijing".equals(cityName)) {
-            if (this.timeSubject.isDstChanged()) {
-                return Integer.toString(8);
-            } else {
-                return Integer.toString(9);
-            }
-        }
-        if ("Moscow".equals(cityName)) {
-            if (this.timeSubject.isDstChanged()) {
-                return Integer.toString(4);
-            } else {
-                return Integer.toString(5);
-            }
-        }
-        if ("Sydney".equals(cityName)) {
-            if (this.timeSubject.isDstChanged()) {
-                return Integer.toString(10);
-            } else {
-                return Integer.toString(11);
-            }
-        }
-        if ("London".equals(cityName)) {
-            if (this.timeSubject.isDstChanged()) {
-                return Integer.toString(0);
-            } else {
-                return Integer.toString(2);
-            }
-        }
-        if ("New York".equals(cityName)) {
-            if (this.timeSubject.isDstChanged()) {
-                return Integer.toString(20);
-            } else {
-                return Integer.toString(21);
-            }
-        }
-        return Integer.toString(2);
+        return this.hotelWorldClocksFacade.getResultHourOfTime(cityName);
     }
 
     public void setCityDstInactive(String cityName) {
-        this.cityBuffer = City.newInstance(cityName);
-        this.cityBuffer.setDstInactive();
-        this.dstCities.add(this.cityBuffer);
+        this.hotelWorldClocksFacade.addACityWithDstStatus(cityName, DaylightSavingTime.INACTIVE);
     }
 
     public void adjustTimeOfCityWhenDstEnds(int hourOfTime, String cityName) {
-        this.timeSubject.setDstChanged(true);
+        this.hotelWorldClocksFacade.adjustTimeOfCityWhenDstChanged(hourOfTime, cityName, DaylightSavingTime.INACTIVE);
     }
 }
